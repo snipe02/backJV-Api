0953fdc3a244570df83479b1037ddb9313b88c97
lucas_mestre <108341139+snipe02@users.noreply.github.com>
2026-09-22 16:13:35 -0300

    ok

--- src/main/java/br/com/palm/devshowcase/controller/TechnologyController.java
+++ src/main/java/br/com/palm/devshowcase/controller/TechnologyController.java
@@ -4,21 +4,54 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import br.com.palm.devshowcase.dto.TechnologyResponseDTO;
-import br.com.palm.devshowcase.service.TechnologyService;
+import br.com.palm.devshowcase.dto.ProjectRequestDTO;
+import br.com.palm.devshowcase.dto.ProjectResponseDTO;
+import br.com.palm.devshowcase.dto.ProfileRequestDTO;
+import br.com.palm.devshowcase.dto.ProfileResponseDTO;
+import br.com.palm.devshowcase.dto.FeedbackRequestDTO;
+import br.com.palm.devshowcase.service.ProjectService;
+import br.com.palm.devshowcase.service.ProfileService;
 
-@RestController @RequestMapping("/api/technologies")
-public class TechnologyController {
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
 
-	@Autowired
-	private TechnologyService service;
-	
-	@GetMapping
-    public ResponseEntity<List<TechnologyResponseDTO>> buscarTodos() {
-        return ResponseEntity.ok(service.buscarTodos());
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponseDTO>> listarProjetos() {
+        return ResponseEntity.ok(projectService.listarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectResponseDTO> criarProjeto(@RequestBody ProjectRequestDTO dto) {
+        return ResponseEntity.ok(projectService.criar(dto));
+    }
+
+    @PostMapping("/{id}/feedbacks")
+    public ResponseEntity<Void> adicionarFeedback(@PathVariable Long id, @RequestBody FeedbackRequestDTO dto) {
+        projectService.adicionarFeedback(id, dto);
+        return ResponseEntity.ok().build();
+    }
+}
+
+@RestController
+@RequestMapping("/api/profiles")
+public class ProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfileResponseDTO> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(profileService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfileResponseDTO> criarProfile(@RequestBody ProfileRequestDTO dto) {
+        return ResponseEntity.ok(profileService.criar(dto));
     }
 }
